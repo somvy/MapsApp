8c05865a4193846bd63ce960afddccb056d5f7d2
somvy <45141221+somvy@users.noreply.github.com>
2019-04-04 21:57:13 +0300

    Add files via upload

--- t1.py
+++ t1.py
@@ -12,22 +12,30 @@ class MyApplication(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.scale = '0.3'
+        self.scale = 0.3
         self.x_coords = 37.622504
         self.y_coords = 55.753215
         self.maps_api_server = "https://static-maps.yandex.ru/1.x/"
         self.painter = QPainter()
         #self.pushButton_Back.clicked.connect(self.back)
-        #self.pushButton_Minus.clicked.connect(self.minus)
-        #self.pushButton_Plus.clicked.connect(self.plus)
+        self.pushButton_Minus.clicked.connect(self.minus)
+        self.pushButton_Plus.clicked.connect(self.plus)
         #self.pushButton_Search.clicked.connect(self.search)
         self.update()
+
+    def keyPressEvent(self, e):
+
+        if e.key() == Qt.Key_PageUp:
+            self.plus()
+        elif e.key() == Qt.Key_PageDown:
+            self.minus()
 
     def update(self):
         maps_params = {
             "ll": ','.join([str(self.x_coords), str(self.y_coords)]),
             "l": 'map',
-            "spn": ','.join([self.scale, self.scale])
+            "spn": ','.join([str(self.scale), str(self.scale)]),
+            "size":'650,450'
         }
         maps_response = requests.get(self.maps_api_server, params=maps_params)
         map_file = "map.png"
@@ -39,6 +47,25 @@ class MyApplication(QMainWindow, Ui_MainWindow):
             sys.exit(2)
         self.pixmap = QPixmap('map.png')
         self.label.setPixmap(self.pixmap)
+        print('update')
+
+    def minus(self):
+        max_scale = 50
+        new_scale = self.scale*1.2
+        if new_scale >= max_scale:
+            self.scale = max_scale
+        else:
+            self.scale = new_scale
+        self.update()
+
+    def plus(self):
+        min_scale = 0.005
+        new_scale = self.scale*0.8
+        if new_scale <= min_scale:
+            self.scale = min_scale
+        else:
+            self.scale = new_scale
+        self.update()
 
 
 if __name__ == '__main__':
