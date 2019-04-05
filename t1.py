f8d18e3e59d10e28dbe930e7828babb29fdb6b06
somvy <45141221+somvy@users.noreply.github.com>
2019-04-05 18:21:04 +0300

    Add files via upload

--- t1.py
+++ t1.py
@@ -15,12 +15,19 @@ class MyApplication(QMainWindow, Ui_MainWindow):
         self.scale = 0.3
         self.x_coords = 37.622504
         self.y_coords = 55.753215
+        self.point_x = 37.622504
+        self.point_y = 55.753215
+        self.map_type = 'map'
         self.maps_api_server = "https://static-maps.yandex.ru/1.x/"
+        self.coder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+        self.ispoint = False
         self.painter = QPainter()
-        #self.pushButton_Back.clicked.connect(self.back)
+
+        # self.pushButton_Back.clicked.connect(self.back)
         self.pushButton_Minus.clicked.connect(self.minus)
         self.pushButton_Plus.clicked.connect(self.plus)
-        #self.pushButton_Search.clicked.connect(self.search)
+        self.pushButton_Search.clicked.connect(self.search)
+        self.pushButton_Change.clicked.connect(self.change)
         self.update()
 
     def keyPressEvent(self, e):
@@ -37,30 +44,43 @@ class MyApplication(QMainWindow, Ui_MainWindow):
             self.right()
         elif e.key() == Qt.Key_Left:
             self.down()
-
+        elif e.key() == Qt.Key_Enter:
+            self.search()
 
     def update(self):
-        maps_params = {
-            "ll": ','.join([str(self.x_coords), str(self.y_coords)]),
-            "l": 'map',
-            "spn": ','.join([str(self.scale), str(self.scale)]),
-            "size": '650,450'
-        }
+        if self.ispoint:
+            maps_params = {
+                "ll": ','.join([str(self.x_coords), str(self.y_coords)]),
+                "l": self.map_type,
+                "spn": ','.join([str(self.scale), str(self.scale)]),
+                "size": '650,450',
+                "pt": '{},{},pm2dgm'.format(str(self.point_x), str(self.point_y))
+            }
+        else:
+            maps_params = {
+                "ll": ','.join([str(self.x_coords), str(self.y_coords)]),
+                "l": self.map_type,
+                "spn": ','.join([str(self.scale), str(self.scale)]),
+                "size": '650,450'
+            }
         maps_response = requests.get(self.maps_api_server, params=maps_params)
-        map_file = "map.png"
+        if self.map_type != 'map':
+            map_file = "map.jpg"
+        else:
+            map_file = 'map.png'
         try:
             with open(map_file, "wb") as file:
                 file.write(maps_response.content)
         except IOError as ex:
             print("Ошибка записи временного файла:", ex)
             sys.exit(2)
-        self.pixmap = QPixmap('map.png')
+        self.pixmap = QPixmap(map_file)
         self.label.setPixmap(self.pixmap)
-        print('update')
+        print('update', self.pixmap.isNull())
 
     def minus(self):
         max_scale = 50
-        new_scale = self.scale*1.2
+        new_scale = self.scale * 1.2
         if new_scale >= max_scale:
             self.scale = max_scale
         else:
@@ -69,7 +89,7 @@ class MyApplication(QMainWindow, Ui_MainWindow):
 
     def plus(self):
         min_scale = 0.005
-        new_scale = self.scale*0.8
+        new_scale = self.scale * 0.8
         if new_scale <= min_scale:
             self.scale = min_scale
         else:
@@ -89,8 +109,38 @@ class MyApplication(QMainWindow, Ui_MainWindow):
         self.update()
 
     def down(self):
-        self.y_coords -= 0.1
+        self.y_coords = float(self.y_coords) - 0.1
         self.update()
+
+    def change(self):
+        map_types = ['map', 'sat', 'sat,skl']
+        try:
+            self.map_type = map_types[map_types.index(self.map_type) + 1]
+        except IndexError:
+            self.map_type = 'map'
+        self.update()
+
+    def search(self):
+        self.ispoint = True
+        text = self.lineEditSearch.text()
+        coder_params = {
+            "geocode": text,
+            "format": 'json'
+        }
+        coder_response = requests.get(self.coder_api_server, params=coder_params)
+        json_response = coder_response.json()
+        coords = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]['Point']['pos'].split()
+        print(coords)
+        x, y = coords[0], coords[1]
+        self.point_x = x
+        self.point_y = y
+        self.x_coords = x
+        self.y_coords = y
+        self.update()
+
+        print(x)
+
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
